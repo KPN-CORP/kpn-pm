4579dee4b7cb3b5935f34613050c5c8b2bbea6f8
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2024-09-11 15:23:17 +0700

    optimization code

--- resources/views/layouts_/shared/left-sidebar.blade.php
+++ resources/views/layouts_/shared/left-sidebar.blade.php
@@ -76,8 +76,7 @@
             @endif
             <li class="side-nav-item">
                 <a data-bs-toggle="collapse" href="#sidebarGoals" aria-expanded="false" aria-controls="sidebarGoals" class="side-nav-link">
-                    <i class="ri-star-line"></i>
-                    {{-- <span class="badge bg-success float-end">2</span> --}}
+                    <i class="ri-focus-2-line"></i>
                     <span>{{ __('Goal') }}</span>
                     <span class="menu-arrow"></span>
                 </a>
@@ -96,8 +95,7 @@
             </li>
             <li class="side-nav-item">
                 <a data-bs-toggle="collapse" href="#sidebarAppraisal" aria-expanded="false" aria-controls="sidebarAppraisal" class="side-nav-link">
-                    <i class="ri-star-line"></i>
-                    {{-- <span class="badge bg-success float-end">2</span> --}}
+                    <i class="ri-list-check-3"></i>
                     <span>{{ __('Appraisal') }}</span>
                     <span class="menu-arrow"></span>
                 </a>
@@ -134,72 +132,69 @@
             </li>
 
             @if(auth()->check())
-            @can('adminmenu')
-            <li class="side-nav-title">Admin</li>
+                @can('adminmenu')
+                <li class="side-nav-title">Admin</li>
+                @can('viewsetting')
+                <li class="side-nav-item">
+                    <a data-bs-toggle="collapse" href="#sidebarSettings" aria-expanded="false" aria-controls="sidebarSettings" class="side-nav-link">
+                        <i class="ri-admin-line"></i>
+                        <span> Admin Settings </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarSettings">
+                        <ul class="side-nav-second-level">
+                            @can('viewschedule')
+                            <li>
+                                <a href="{{ route('schedules') }}">Schedule</a>
+                            </li>
+                            @endcan
+                            @can('viewlayer')
+                            <li class="side-nav-item">
+                                <a data-bs-toggle="collapse" href="#sidebarLayer" aria-expanded="false" aria-controls="sidebarLayer">
+                                    <span> Layer </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <div class="collapse" id="sidebarLayer">
+                                    <ul class="side-nav-third-level">
+                                        @can('viewschedule')
+                                        <li>
+                                            <a href="{{ route('layer') }}">Goals</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('layer-appraisal') }}">{{ __('Appraisal') }}</a>
+                                        </li>
+                                        @endcan
+                                    </ul>
+                                </div>
+                            </li>
+                            @endcan
+                            @can('viewrole')
+                            <li>
+                                <a href="{{ route('roles') }}">Role</a>
+                            </li>
+                            @endcan
+                        </ul>
+                    </div>
+                </li>
+                @endcan
+                @can('viewonbehalf')
+                <li class="side-nav-item">
+                    <a href="{{ route('onbehalf') }}" class="side-nav-link">
+                        <i class="ri-group-line"></i>
+                        <span> On Behalfs </span>
+                    </a>
+                </li>
+                @endcan
+                @can('viewreport')
+                <li class="side-nav-item">
+                    <a href="{{ route('admin.reports') }}" class="side-nav-link">
+                        <i class="ri-file-chart-line"></i>
+                        <span> {{ __('Report') }} </span>
+                    </a>
+                </li>
+                @endcan
 
-            <li class="side-nav-item">
-                <a data-bs-toggle="collapse" href="#sidebarCharts" aria-expanded="false" aria-controls="sidebarCharts" class="side-nav-link">
-                    <i class="ri-admin-line"></i>
-                    <span> Admin Menu </span>
-                    <span class="menu-arrow"></span>
-                </a>
-                <div class="collapse" id="sidebarCharts">
-                    <ul class="side-nav-second-level">
-                        @can('viewsetting')
-                        <li class="side-nav-item">
-                            <a data-bs-toggle="collapse" href="#sidebarApexCharts" aria-expanded="false" aria-controls="sidebarApexCharts">
-                                <span> Settings </span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <div class="collapse" id="sidebarApexCharts">
-                                <ul class="side-nav-third-level">
-                                    @can('viewschedule')
-                                    <li>
-                                        <a href="{{ route('schedules') }}">Schedule</a>
-                                    </li>
-                                    @endcan
-                                    @can('viewlayer')
-                                    <li>
-                                        <a data-bs-toggle="collapse" href="#sidebarLayer" aria-expanded="false" aria-controls="sidebarLayer" class="side-nav-link">
-                                            <span> Layer </span>
-                                            <span class="menu-arrow"></span>
-                                        </a>
-                                        <div class="collapse" id="sidebarLayer">
-                                            <ul class="side-nav-second-level">
-                                                <li>
-                                                    <a href="{{ route('layer') }}">Goals</a>
-                                                </li>
-                                                <li>
-                                                    <a href="{{ route('layer-appraisal') }}">{{ __('Appraisal') }}</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    @endcan
-                                    @can('viewrole')
-                                    <li>
-                                        <a href="{{ route('roles') }}">Role</a>
-                                    </li>
-                                    @endcan
-                                </ul>
-                            </div>
-                        </li>
-                        @endcan
-                        @can('viewonbehalf')
-                        <li class="side-nav-item">
-                            <a  href="{{ route('onbehalf') }}">On Behalfs</a>
-                        </li>
-                        @endcan
-                        @can('viewreport')
-                        <li class="side-nav-item">
-                            <a href="{{ route('admin.reports') }}">{{ __('Report') }}</a>
-                        </li>
-                        @endcan
-                    </ul>
-                </div>
-            </li>
-
-            @endcan
+                @endcan
             @endif
 
         </ul>
