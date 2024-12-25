e8efe191047c877ed165475fc2742c1391548ac9
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2024-12-26 00:09:33 +0700

    enhance ui create/initiate PA

--- resources/views/pages/appraisals/create.blade.php
+++ resources/views/pages/appraisals/create.blade.php
@@ -10,7 +10,7 @@
         <div class="row justify-content-center">
             <div class="col">
                 <div class="card">
-                    <div class="card-header d-flex justify-content-md-center">
+                    <div class="card-header d-flex justify-content-center">
                         <div class="col-md-10 text-center">
                             <div class="stepper mt-3 d-flex justify-content-between justify-content-md-around">
                                 @foreach ($filteredFormData as $index => $tabs)
