e0aa30be0f2e0ce09b33f757cdb99370d536a7e8
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2025-03-20 13:25:18 +0700

    Update TS 2025 alert text

--- resources/js/goal-approval.js
+++ resources/js/goal-approval.js
@@ -207,7 +207,7 @@ function sendBack(id, nik, name) {
 
     let title1 = "Confirm you want to send back?";
     let title2 = "KPI sendback successfuly!";
-    let text = `The goal will be send back to ${name}`;
+    let text = `The goals will be sent back to ${name}`;
     let confirmText = "Submit";
 
     Swal.fire({
