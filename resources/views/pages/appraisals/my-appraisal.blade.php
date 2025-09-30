81b40721e5f8d6d52e8f6e787355ddda3554c961
eritonld <eritonld@gmail.com>
2025-09-30 13:43:51 +0700

    update view app 2

--- resources/views/pages/appraisals/my-appraisal.blade.php
+++ resources/views/pages/appraisals/my-appraisal.blade.php
@@ -166,17 +166,23 @@
                                 <button class="btn rounded mb-2 py-2 bg-secondary-subtle bg-opacity-10 text-primary align-items-center d-flex justify-content-between" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{ $indexItem }}" aria-expanded="false" aria-controls="collapse-{{ $indexItem }}">
                                     <span class="fs-16 ms-1">
                                         {{ $item['formName'] }} 
-                                        | Score : {{ $item['formName'] === 'KPI' ? $appraisalData['kpiScore'] : ($item['formName'] === 'Culture' ? $appraisalData['cultureScore'] : $appraisalData['leadershipScore']) }}
+                                        | Score : {{ 
+                                                $item['formName'] === 'KPI' ? $appraisalData['kpiScore'] :
+                                                ($item['formName'] === 'Culture' ? $appraisalData['cultureScore'] :
+                                                ($item['formName'] === 'Leadership' ? $appraisalData['leadershipScore'] :
+                                                ($item['formName'] === 'Leadership 1' ? $appraisalData['leadershipScore1'] :
+                                                ($item['formName'] === 'Leadership 2' ? $appraisalData['leadershipScore2'] : ''))))
+                                            }}
                                     </span>  
                                     <span>
                                         <p class="d-none d-md-inline me-1">Details</p><i class="ri-arrow-down-s-line"></i>
                                     </span>                               
                                 </button>
-                                @if ($item['formName'] == 'Leadership' || $item['formName'] == 'Leadership 1' || $item['formName'] == 'Leadership 2')
+                                @if ($item['formName'] == 'Leadership')
                                 <div class="collapse" id="collapse-{{ $indexItem }}">
                                     <div class="card card-body mb-3">
                                         @forelse($formData['formData'] as $form)
-                                            @if($form['formName'] === 'Leadership' || $form['formName'] === 'Leadership 1' || $form['formName'] === 'Leadership 2')
+                                            @if($form['formName'] === 'Leadership')
                                                 @foreach($form as $key => $item)
                                                     @if(is_numeric($key))
                                                     <div class="{{ $loop->last ? '':'border-bottom' }} mb-3">
@@ -213,6 +219,78 @@
                                     <div class="card card-body mb-3">
                                         @forelse($formData['formData'] as $form)
                                             @if($form['formName'] === 'Culture')
+                                                @foreach($form as $key => $item)
+                                                    @if(is_numeric($key))
+                                                    <div class="{{ $loop->last ? '':'border-bottom' }} mb-3">
+                                                        @if(isset($item['title']))
+                                                            <h5 class="mb-3"><u>{{ $item['title'] }}</u></h5>
+                                                        @endif
+                                                        @foreach($item as $subKey => $subItem)
+                                                            @if(is_array($subItem))
+                                                            <ul class="ps-3">
+                                                                <li>
+                                                                    <div>
+                                                                        @if(isset($subItem['formItem']))
+                                                                            <p class="mb-1">{!! $subItem['formItem'] !!}</p>
+                                                                        @endif
+                                                                        @if(isset($subItem['score']))
+                                                                            <p><strong>Score:</strong> {{ $subItem['score'] }}</p>
+                                                                        @endif
+                                                                    </div>
+                                                                </li>
+                                                            </ul>
+                                                            @endif
+                                                        @endforeach
+                                                    </div>
+                                                    @endif
+                                                @endforeach
+                                            @endif
+                                        @empty
+                                            <p>No Data</p>
+                                        @endforelse
+                                    </div>
+                                </div>
+                                @elseif($item['formName'] == 'Leadership 1')
+                                <div class="collapse" id="collapse-{{ $indexItem }}">
+                                    <div class="card card-body mb-3">
+                                        @forelse($formData['formData'] as $form)
+                                            @if($form['formName'] === 'Leadership 1')
+                                                @foreach($form as $key => $item)
+                                                    @if(is_numeric($key))
+                                                    <div class="{{ $loop->last ? '':'border-bottom' }} mb-3">
+                                                        @if(isset($item['title']))
+                                                            <h5 class="mb-3"><u>{{ $item['title'] }}</u></h5>
+                                                        @endif
+                                                        @foreach($item as $subKey => $subItem)
+                                                            @if(is_array($subItem))
+                                                            <ul class="ps-3">
+                                                                <li>
+                                                                    <div>
+                                                                        @if(isset($subItem['formItem']))
+                                                                            <p class="mb-1">{!! $subItem['formItem'] !!}</p>
+                                                                        @endif
+                                                                        @if(isset($subItem['score']))
+                                                                            <p><strong>Score:</strong> {{ $subItem['score'] }}</p>
+                                                                        @endif
+                                                                    </div>
+                                                                </li>
+                                                            </ul>
+                                                            @endif
+                                                        @endforeach
+                                                    </div>
+                                                    @endif
+                                                @endforeach
+                                            @endif
+                                        @empty
+                                            <p>No Data</p>
+                                        @endforelse
+                                    </div>
+                                </div>
+                                @elseif($item['formName'] == 'Leadership 2')
+                                <div class="collapse" id="collapse-{{ $indexItem }}">
+                                    <div class="card card-body mb-3">
+                                        @forelse($formData['formData'] as $form)
+                                            @if($form['formName'] === 'Leadership 2')
                                                 @foreach($form as $key => $item)
                                                     @if(is_numeric($key))
                                                     <div class="{{ $loop->last ? '':'border-bottom' }} mb-3">
