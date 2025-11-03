1bc4685d65a9d24ad098376cff5884160b30d7a6
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2025-11-03 15:44:04 +0700

    adjust form appraisal

--- resources/views/components/culture.blade.php
+++ resources/views/components/culture.blade.php
@@ -38,7 +38,7 @@
                                                 <option value="">select</option>
                                                 @foreach ($ratings as $item) {{-- $isManager --}}
                                                     <option value="{{ $item['value'] }}" 
-                                                        {{ isset($dataItem['score'][$indexItem]) && $dataItem['score'][$indexItem] == $item['value'] ? 'selected' : '' }}>
+                                                        {{ isset($dataItem['score'][$indexItem]) && (isset($isManager) && $isManager) && $dataItem['score'][$indexItem] == $item['value'] ? 'selected' : '' }}>
                                                         {{ $item['value'] }}
                                                     </option>
                                                 @endforeach
