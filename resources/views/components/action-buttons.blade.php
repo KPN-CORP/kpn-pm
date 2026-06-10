dc0b7b569deeb3a4fbbe473e332cf1fccce78b13
Alfian Azis <85601052+alfian-nfa@users.noreply.github.com>
2026-06-10 16:04:35 +0700

    Update Task HCIS-205

--- resources/views/components/action-buttons.blade.php
+++ resources/views/components/action-buttons.blade.php
@@ -1,25 +1,41 @@
 @php
     $filterYear = request('filterYear');
 @endphp
-@forelse ($team->contributors as $contributor)
-    @if (!$team->calibrationCheck)
-        <a href="{{ route('appraisals-360.review', encrypt($team->employee->employee_id)) }}" title="{{ __('Revise') }}" type="button" class="btn btn-outline-info btn-sm m-1 me-0"><i class="ri-edit-line"></i></a>
-    @endif
-    @if ($contributor->status != 'Draft')
-        <a href="{{ route('appraisals-task.detail', [
-            'id' => encrypt($contributor->id),
-            'year' => $filterYear
-        ]) }}"
-        title="{{ __('Details') }}"
-        type="button"
-        class="btn btn-outline-secondary btn-sm m-1">
-            <i class="ri-file-text-line"></i>
-        </a>
-    @endif
-@empty
-    @if ($team->layer_type === 'manager' && empty(json_decode($team->approvalRequest, true)))
-        <a href="{{ route('appraisals-task.initiate', encrypt($team->employee->employee_id)) }}" type="button" class="btn btn-outline-primary btn-sm m-1">{{ __('Initiate') }}</a>
-    @else
-        <a href="{{ route('appraisals-360.review', encrypt($team->employee->employee_id)) }}" type="button" class="btn btn-outline-warning btn-sm m-1">{{ __('Review') }}</a>
-    @endif
-@endforelse
+@if ($isCurrentPeriod)
+    @forelse ($team->contributors as $contributor)
+        @if (!$team->calibrationCheck)
+            <a href="{{ route('appraisals-360.review', encrypt($team->employee->employee_id)) }}" title="{{ __('Revise') }}" type="button" class="btn btn-outline-info btn-sm m-1 me-0"><i class="ri-edit-line"></i></a>
+        @endif
+        @if ($contributor->status != 'Draft')
+            <a href="{{ route('appraisals-task.detail', [
+                'id' => encrypt($contributor->id),
+                'year' => $filterYear
+            ]) }}"
+            title="{{ __('Details') }}"
+            type="button"
+            class="btn btn-outline-secondary btn-sm m-1">
+                <i class="ri-file-text-line"></i>
+            </a>
+        @endif
+    @empty
+        @if ($team->layer_type === 'manager' && empty(json_decode($team->approvalRequest, true)))
+            <a href="{{ route('appraisals-task.initiate', encrypt($team->employee->employee_id)) }}" type="button" class="btn btn-outline-primary btn-sm m-1">{{ __('Initiate') }}</a>
+        @else
+            <a href="{{ route('appraisals-360.review', encrypt($team->employee->employee_id)) }}" type="button" class="btn btn-outline-warning btn-sm m-1">{{ __('Review') }}</a>
+        @endif
+    @endforelse
+@else
+    @foreach ($team->contributors as $contributor)
+        @if ($contributor->status != 'Draft')
+            <a href="{{ route('appraisals-task.detail', [
+                'id' => encrypt($contributor->id),
+                'year' => $filterYear
+            ]) }}"
+            title="{{ __('Details') }}"
+            type="button"
+            class="btn btn-outline-secondary btn-sm m-1">
+                <i class="ri-file-text-line"></i>
+            </a>
+        @endif
+    @endforeach
+@endif
